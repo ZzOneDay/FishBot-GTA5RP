ec7b2c0f6e42c3877e6da9272af112d38457691c
Pavel Novikov <pavel.novikov@dataart.com>
2022-01-31 13:23:48 +0300

    Task.Core.8 - added my log class

---
+++ src/main/java/home/developer/core/MyLOG.java
@@ -0,0 +1,11 @@
+package home.developer.core;
+
+public class MyLOG {
+    static public void info(String message) {
+        System.out.println(message);
+    }
+
+    static public void error(String message) {
+        System.err.println(message);
+    }
+}
